1dfed3e3a6c6b52200a5e3c599e9f158d23b3a4b
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 17:02:09 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_19_124659) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "message"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_19_124659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "profiles", "users"
 end
