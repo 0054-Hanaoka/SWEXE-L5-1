b175398ff358443faa344d1bbdc108e81f1804d8
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 19:50:02 +0000

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,25 @@
 class LikesController < ApplicationController
   before_action :check_login
 
-  def index
-    # ログイン中ユーザーのいいね一覧
-    user = current_user
-    @liked_tweets = user.like_tweets.includes(:user)
-  end
-
   def create
-    user = current_user
     tweet = Tweet.find(params[:tweet_id])
-    unless user.like_tweets.include?(tweet)
-      user.like_tweets << tweet
-    end
-    redirect_to root_path, notice: "いいねしました"
+    user = User.find_by(uid: session[:login_uid])
+    user.like_tweets << tweet unless user.like_tweets.exists?(tweet.id)
+    redirect_to root_path
   end
 
   def destroy
-    user = current_user
     tweet = Tweet.find(params[:tweet_id])
+    user = User.find_by(uid: session[:login_uid])
     like = tweet.likes.find_by(user_id: user.id)
+
     if like
       like.destroy
       flash[:notice] = "いいねを解除しました"
     else
       flash[:alert] = "いいねが存在しません"
     end
+
     redirect_to root_path
   end
 
@@ -35,10 +29,5 @@ class LikesController < ApplicationController
     unless session[:login_uid]
       redirect_to top_main_path, alert: "ログインしてください"
     end
-  end
-
-  # よく使うのでメソッド化
-  def current_user
-    User.find_by(uid: session[:login_uid])
   end
 end
