533a20a3092faed4a058ca6a44048bf1ee6a1ec2
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 19:17:09 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # new と create はログイン不要、それ以外はログイン必須
+  before_action :require_login, except: [:new, :create]
+
   def index
     if session[:login_uid]
       @user = User.find_by(uid: session[:login_uid])
@@ -6,7 +9,6 @@ class UsersController < ApplicationController
     end
     @users = User.all
   end
-
 
   def new
     @user = User.new
@@ -19,6 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(uid: uid, pass: BCrypt::Password.create(pass))
 
     if @user.save
+      session[:login_uid] = @user.uid  # 登録後に自動ログインさせる場合
       redirect_to users_path, notice: "ユーザーを作成しました"
     else
       render :new, status: 422
@@ -30,8 +33,8 @@ class UsersController < ApplicationController
     u.destroy
     redirect_to users_path, notice: "ユーザーを削除しました"
   end
-  
-  before_action :require_login
+
+  private
 
   def require_login
     unless session[:login_uid]
