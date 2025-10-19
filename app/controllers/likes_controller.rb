ae019f1aea45df4065b2a6053524704ebf2d62af
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 19:34:25 +0000

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,40 +1,44 @@
 class LikesController < ApplicationController
-    def index
-        # ログイン中ユーザーのいいね一覧を取得
-        user = User.find_by(uid: session[:login_uid])
-        @liked_tweets = user.like_tweets.includes(:user)
-    end
+  before_action :check_login
+
+  def index
+    # ログイン中ユーザーのいいね一覧
+    user = current_user
+    @liked_tweets = user.like_tweets.includes(:user)
+  end
 
   def create
+    user = current_user
     tweet = Tweet.find(params[:tweet_id])
-    user = User.find_by(uid: session[:login_uid])
-    user.like_tweets << tweet
-    redirect_to root_path
+    unless user.like_tweets.include?(tweet)
+      user.like_tweets << tweet
+    end
+    redirect_to root_path, notice: "いいねしました"
   end
 
   def destroy
-    tweet = Tweet.find(params[:id])
-    user = User.find_by(uid: session[:login_uid])
+    user = current_user
+    tweet = Tweet.find(params[:tweet_id])
     like = tweet.likes.find_by(user_id: user.id)
-
     if like
       like.destroy
       flash[:notice] = "いいねを解除しました"
     else
       flash[:alert] = "いいねが存在しません"
     end
-
     redirect_to root_path
   end
-  
-  before_action :check_login
 
   private
-  
+
   def check_login
     unless session[:login_uid]
       redirect_to top_main_path, alert: "ログインしてください"
     end
   end
 
+  # よく使うのでメソッド化
+  def current_user
+    User.find_by(uid: session[:login_uid])
+  end
 end
