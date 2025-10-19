c5660a575736d55ad0171995a0b0d7daefc4c512
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 17:17:35 +0000

    修正

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,17 @@ Bundler.require(*Rails.groups)
 
 module L5
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.after_initialize do
+      # 本番環境のみ
+      if Rails.env.production?
+        begin
+          ActiveRecord::Base.connection
+          ActiveRecord::Migrator.migrations_paths = [File.expand_path('db/migrate', __dir__)]
+          ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrate
+        rescue => e
+          Rails.logger.error "Migration failed: #{e.message}"
+        end
+      end
+    end
   end
 end
