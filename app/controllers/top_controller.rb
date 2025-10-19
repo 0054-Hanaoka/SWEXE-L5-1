295553ed2d7a43f75ef6d7cefad16c779785b881
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-19 19:23:01 +0000

    修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,27 +1,33 @@
 class TopController < ApplicationController
+  def login
+    uid = params[:uid]
+    pass = params[:pass]
+  
+    user = User.find_by(uid: uid)
+    if user && BCrypt::Password.new(user.pass) == pass
+      session[:login_uid] = user.uid
+      redirect_to top_main_path
+    else
+      flash.now[:alert] = "IDまたはパスワードが違います"
+      render "login", status: 422
+    end
+  end
+  
   def main
     if session[:login_uid]
       @user = User.find_by(uid: session[:login_uid])
-      @profile = @user.profile  # ← プロフィール情報を取得
-      render "main"
+      if @user
+        @profile = @user.profile
+        render "main"
+      else
+        session.delete(:login_uid)
+        redirect_to top_main_path, alert: "ユーザー情報が見つかりません"
+      end
     else
       render "login"
     end
   end
 
-  def login
-    uid = params[:uid]
-    pass = params[:pass]
-
-    user = User.find_by(uid: uid)
-
-    if user && BCrypt::Password.new(user.pass) == pass
-      session[:login_uid] = uid
-      redirect_to top_main_path  # ← プロフィール取得後の main にリダイレクト
-    else
-      render "error", status: 422
-    end
-  end
 
   def logout
     session.delete(:login_uid)
